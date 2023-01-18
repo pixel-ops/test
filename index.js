defdbd7934b794459fbe8182be8a7fe106909b77
Meet Parekh <opspixel017@gmail.com>
2023-01-18 18:13:46 +0530

    18 jan EOD commit

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-var a ;
-var b ;
-
-function calculator() {
-
-    document.getElementById("one").onclick = write("one");
-
-    // if()
+let ans = document.getElementById('inp').value;  
+var value = "";
+function myfunction(event){
+    value += event.target.value;
+    document.getElementById("inp").value = value;
     
+
+}
+
+function evaluatess(){
+    ans = eval(value);
+    console.log(ans,value);
+    document.getElementById("inp").value = ans;
 }
 
 
-function add(){    
-    a = parseInt(document.getElementById("1").value);
-    b = parseInt(document.getElementById("1").value);
-    let c = a + b;
-    document.getElementById("ans").innerHTML = c;
-}
-
-function sub(){    
-    a = parseInt(document.getElementById("1").value);
-    b = parseInt(document.getElementById("2").value);
-    let c = a - b;
-    document.getElementById("ans").innerHTML = c;
-}
-
-function mul(){    
-    a = parseInt(document.getElementById("1").value);
-    b = parseInt(document.getElementById("2").value);
-    let c = a * b;
-    document.getElementById("ans").innerHTML = c;
-}
-
-function div(){    
-    a = parseInt(document.getElementById("1").value);
-    b = parseInt(document.getElementById("2").value);
-    let c = Number(a / b);
-    document.getElementById("ans").innerHTML = c;
-}
