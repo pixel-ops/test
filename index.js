f02e3a33b3ab87d7f63b2e00f5f19cdfe25f6efb
Meet Parekh <opspixel017@gmail.com>
2023-01-20 17:48:21 +0530

    Final push complete project

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ let ans = document.getElementById('inp').value;
 var value = "";
 var mem = 0;
 
-//from stack overflow let reg = /^\s*([-+]?)(\d+)(?:\s*([-+*\/])\s*((?:\s[-+])?\d+)\s*)+$/;
-//from other website let reg = /^(?!\.)(?!.*\.$)(?!.*\.\.)[a-zA-Z0-9_.]+$/;
-// from chatgpt let reg = /^[\d()+\-*/\s]*(\*\*[\d()+\-*/\s]*)*$/;
+// let reg = /^\s*([-+]?)(\d+)(?:\s*([-+*\/])\s*((?:\s[-+])?\d+)\s*)+$/;
+// from other website let reg = /^(?!\.)(?!.*\.$)(?!.*\.\.)[a-zA-Z0-9_.]+$/;
+// let reg = /^[\d()+\-*/\s]*(\*\*[\d()+\-*/\s]*)*$/;
+// let reg = /^(?:\d+\.\d*|\.\d+|\d+|[+\-*/^()]|\*\*)+$/;
+// let reg = /^(-?\d+.?\d*)([/+-**])(-?\d+.?\d*)$/;
+// let reg =/^\d+((\.\d+)?[\+\-×÷]\d+(\.\d+)?)+$/
+let reg = /^[\d\+\-\*\/\(\)\.]*(\*\*[\d\+\-\*\/\(\)\.]+)*$/;
 
 //main function that takes input
 function myfunction(event){
     value += event.target.value;
     document.getElementById("inp").value = value;
 }
+// function to check string for input validation using regex
+function check_str(){
+    console.log("in check_str");
+    if(value.match(reg)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 //function that evaluates the expression 
 function evaluatess(){
     if(value != ""){
-        // if(value.match(reg)){
+        // if(check_str()){
             ans = eval(value);
-            document.getElementById("inp").value = ans;
-            value = ans;
+            document.getElementById("inp").value = ans.toPrecision(13);
+            value = ans.toPrecision(13); 
+
         // }
         // else{
         //     document.getElementById('inp').value = "Error";
@@ -26,61 +42,109 @@ function evaluatess(){
 }
 //clear the input field
 function clear_me(){
-    document.getElementById("inp").value = "";
+    document.getElementById("inp").value = 0;
     value = ""; 
 }
 //delete the last element in input field
 function delete_last(){
     document.getElementById("inp").value = value.slice(0,value.length-1);
     value = value.slice(0,value.length-1);
+    if(value == ""){
+        document.getElementById("inp").value = 0;
+        value = "";
+    }
 }
 //square function
 function sqr(){
-    console.log(value);
-    document.getElementById("inp").value = parseFloat(value)*parseFloat(value);
-    value = parseFloat(value)*parseFloat(value);
+    document.getElementById("inp").value = (parseFloat(value)*parseFloat(value)).toPrecision(13);
+    value = (parseFloat(value)*parseFloat(value)).toPrecision(13);
 }
+// function for degree function
+function DEG(event){
+    val = document.getElementById("inp").value
+       if(val != ""){
+           document.getElementById("inp").value = eval((parseInt(value*22)/1260).toPrecision(13));
+           value = eval((parseInt(value*22)/1260).toPrecision(13));
+       }
+       else{
+           document.getElementById("inp").value = "0.01746";
+           value = "0.01746";
+       }
+}
+
+// function for fixed exponent
+function F_E(){
+    document.getElementById("inp").value = parseFloat(value).toExponential(2);
+    value = parseFloat(value).toExponential(2);
+}
+
+
+// check value of input and accordingly put value of pi
+function pi_check(event){
+    val = document.getElementById("inp").value
+    console.log(val);
+       if(val != 0){
+           document.getElementById("PI").value = "*3.14";
+           myfunction(event);
+       }
+       else{
+           document.getElementById("PI").value = "3.14";
+           myfunction(event);
+       }
+}
+// check value of input and accordingly put value of e
+function e_check(event){
+    val = document.getElementById("inp").value
+       if(val != 0  ){
+           document.getElementById("e").value = "*2.71";
+           myfunction(event);
+       }
+       else{
+           document.getElementById("e").value = "2.71";
+           myfunction(event);
+       }
+}
+
+
 //reciprocal function
 function reciprocal(){
-    document.getElementById("inp").value = eval(1/parseFloat(value));
-    value = eval(1/parseFloat(value));
+    document.getElementById("inp").value = eval(1/parseFloat(value)).toPrecision(13);
+    value = eval(1/parseFloat(value)).toPrecision(13);
 }
 //to get absolute value
 function myabs(){
-    document.getElementById("inp").value = Math.abs(parseFloat(value));
-    value = Math.abs(parseFloat(value));
+    document.getElementById("inp").value = Math.abs(parseFloat(value)).toPrecision(13);
+    value = Math.abs(parseFloat(value)).toPrecision(13);
 }
 //find e^value
 function exponent(){
-    document.getElementById("inp").value = Math.exp(parseFloat(value));
-    value = Math.exp(parseFloat(value));
+    document.getElementById("inp").value = Math.exp(parseFloat(value)).toPrecision(13);
+    value = Math.exp(parseFloat(value)).toPrecision(13);
 }
 //find square root
 function squarert(){
-    document.getElementById("inp").value = Math.sqrt(parseFloat(value));
-    value = Math.sqrt(parseFloat(value));
-    console.log(eval(2**3));
+    document.getElementById("inp").value = Math.sqrt(parseFloat(value)).toPrecision(13);
+    value = Math.sqrt(parseFloat(value)).toPrecision(13);
 }
 // finds 10^value
 function ten_raise_to_x(){
-    document.getElementById("inp").value = Math.pow(10,parseFloat(value));
-    value = Math.pow(10,value);
+    document.getElementById("inp").value = Math.pow(10,parseFloat(value)).toPrecision(13);
+    value = Math.pow(10,value).toPrecision(13);
 }
 // finds logarthim
 function logarithm(){
-    document.getElementById("inp").value = Math.log(parseFloat(value));
-    value = Math.log(parseFloat(value));
+    document.getElementById("inp").value = Math.log(parseFloat(value)).toPrecision(13);
+    value = Math.log(parseFloat(value)).toPrecision(13);
 }
 // finds natural log
 function natural_log(){
-    e = 2.71828182846
+    e = 2.71828182846;
     document.getElementById("inp").value = Math.log(e,parseFloat(value));
 }
 
 // function to save the value to value to memory
 function store_mem(){
     mem = value;
-    console.log(mem);
 }
 //recall memory value
 function recall_mem(){
@@ -115,23 +179,35 @@ function factorialize(num) {
 // factorial calling function
 function fact(){
     document.getElementById("inp").value = factorialize(parseInt(value));
+    value = factorialize(parseInt(value)).toPrecision(13);
 }
-
+//sin function
 function sin(){
-    document.getElementById("inp").value = Math.sin((parseInt(value)*Math.PI)/180);
+    document.getElementById("inp").value = Math.sin((parseInt(value)*Math.PI)/180).toPrecision(13);
+    value = Math.sin((parseInt(value)*Math.PI)/180).toPrecision(13);
 }
+// cos function
 function cos(){
-    document.getElementById("inp").value = Math.cos((parseInt(value)*Math.PI)/180);
+    document.getElementById("inp").value = Math.cos((parseInt(value)*Math.PI)/180).toPrecision(13);
+    value = Math.cos((parseInt(value)*Math.PI)/180).toPrecision(13);
 }
+// tan function
 function tan(){
-    document.getElementById("inp").value = Math.tan((parseInt(value)*Math.PI)/180);
+    document.getElementById("inp").value = Math.tan((parseInt(value)*Math.PI)/180).toPrecision(13);
+    value = Math.tan((parseInt(value)*Math.PI)/180).toPrecision(13);
 }
+// sec function
 function sec(){
-    document.getElementById("inp").value = 1/(Math.cos((parseInt(value)*Math.PI)/180));
+    document.getElementById("inp").value = 1/(Math.cos((parseInt(value)*Math.PI)/180)).toPrecision(13);
+    value = 1/(Math.cos((parseInt(value)*Math.PI)/180)).toPrecision(13);
 }
+// cosec function
 function cosec(){
-    document.getElementById("inp").value = 1/(Math.sin((parseInt(value)*Math.PI)/180));
+    document.getElementById("inp").value = 1/(Math.sin((parseInt(value)*Math.PI)/180)).toPrecision(13);
+    value = 1/(Math.sin((parseInt(value)*Math.PI)/180)).toPrecision(13);
 }
+// cot function
 function cot(){
-    document.getElementById("inp").value = 1/(Math.tan((parseInt(value)*Math.PI)/180));
+    document.getElementById("inp").value = 1/(Math.tan((parseInt(value)*Math.PI)/180)).toPrecision(13);
+    value = 1/(Math.tan((parseInt(value)*Math.PI)/180)).toPrecision(13);
 }
